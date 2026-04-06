13311fe7e714abf16dd05b3d28294482f69f869c
Mk-mstar <mkperez.contato@gmail.com>
2026-04-06 15:14:33 -0300

    Versão Final
    Ajustei o código e coloquei oque faltava.

--- Monitorlog.py
+++ Monitorlog.py
@@ -1,29 +1,99 @@
 import datetime
 import random
-import time
 
 def gerar_logs():
     print("== GERADOR DE LOGS ==")
-    
-    for i in range(10):
+
+    arquivo = open("log.txt", "w")
+
+    for i in range(20):
         tempo = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-        ip = f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
-        
-        print(f"[{tempo}] {ip}")
+
+        ip = f"{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}"
+
+        status = random.choice([200, 200, 200, 404, 500])
+        tempo_resp = random.randint(50, 2000)
+
+        linha = f"[{tempo}] {ip} - {status} - {tempo_resp}ms\n"
+
+        arquivo.write(linha)
+
+    arquivo.close()
+
+    print("Logs gerados e salvos em log.txt")
+
+
+def separar(linha):
+    partes = []
+    atual = ""
+
+    for c in linha:
+        if c == " ":
+            if atual != "":
+                partes.append(atual)
+                atual = ""
+        else:
+            atual += c
+
+    if atual != "":
+        partes.append(atual)
+
+    return partes
+
 
 def analisar_logs():
     print("== ANALISADOR DE LOGS ==")
+
+    try:
+        arquivo = open("log.txt", "r")
+    except:
+        print("Arquivo não encontrado! Gere os logs primeiro.")
+        return
+
+    total = 0
+    erros = 0
+    lentos = 0
+
+    for linha in arquivo:
+        total += 1
+
+        partes = separar(linha)
+
+        ip = partes[2]
+        status = int(partes[4])
+        tempo = int(partes[6].replace("ms", ""))
+
+        if status >= 500:
+            erros += 1
+
+        if tempo > 1000:
+            lentos += 1
+
+    arquivo.close()
+
+    print("\n=== RELATÓRIO ===")
+    print("Total de logs:", total)
+    print("Erros (500):", erros)
+    print("Requisições lentas:", lentos)
+
+
+    if erros > 5:
+        print(" Sistema CRÍTICO")
+    elif lentos > 5:
+        print(" Sistema em ATENÇÃO")
+    else:
+        print(" Sistema SAUDÁVEL")
+
 
 def gerar_e_analisar_logs():
     gerar_logs()
     analisar_logs()
 
 def sair():
-    print("== SAIR ==")
+    print("Encerrando sistema...")
 
 def menu():
-    print(' == Menu ==')
-    nome_arq = 'log.txt'
+    print('\n== MENU ==')
     print('1: Gerar logs')
     print('2: Analisar logs')
     print('3: Gerar e analisar logs')
@@ -31,7 +101,12 @@ def menu():
 
 while True:
     menu()
-    escolha = int(input('Escolha uma opção: '))
+    
+    try:
+        escolha = int(input('Escolha uma opção: '))
+    except:
+        print("Digite um número válido")
+        continue
 
     if escolha == 1:
         gerar_logs()
